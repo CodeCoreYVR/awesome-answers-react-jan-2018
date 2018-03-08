61edaf26c4527952859aed6d7f261eab49badbfe
Steve Godin <steve.god@gmail.com>
2018-03-08 14:24:22 -0800

    Add demo for Delete for Questions Index

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -11,6 +11,38 @@ class QuestionIndexPage extends React.Component {
     this.state = {
       questions: questionsData
     }
+
+    // When using a method as a callback, we must bind
+    // this to otherwise we won't have access to any properties
+    // of `this` include the state, setState and the props.
+
+    // `.bind()` is a method of functions that effectively
+    // creates new function that is copy of the function
+    // where `this` is bound permanently.
+    this.deleteQuestion = this.deleteQuestion.bind(this)
+  }
+
+  deleteQuestion (event) {
+    const {currentTarget} = event
+    // console.log(currentTarget.dataset.id)
+
+    const {questions} = this.state;
+    const questionId = parseInt(currentTarget.dataset.id);
+    console.log(questionId)
+    // To delete a question, will have to update the state
+    // to version of the state where that question is no longer
+    // present.
+    this.setState({
+      questions: questions
+        .filter(question => question.id !== questionId)
+    })
+    // Everytime you want to change the state, use the this.setState()
+    // method. This will notify React that it potentially needs
+    // to update the DOM based on the new data in the state.
+
+    // Modifying this.state directly will cause headaches and not
+    // reflect any changes to the DOM.
+    // DO NOT DO IT! 😱
   }
 
   render () {
@@ -29,6 +61,10 @@ class QuestionIndexPage extends React.Component {
                   <li key={question.id}>
                     <a href="">{question.title}</a>
                     <Field name="Author" value={question.author.full_name} />
+                    <button
+                      data-id={question.id}
+                      onClick={this.deleteQuestion}
+                    >Delete</button>
                   </li>
                 )
               )
