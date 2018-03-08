1000d3e96ebdb5dc63b723acd7ea5b88852857a4
Steve Godin <steve.god@gmail.com>
2018-03-08 13:36:58 -0800

    Add demo for Using State

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+// React is default import.
+// Component (which must import with {}) is named import.
+
+// To export a default, React has to `export default React`
+// To export a named import, React does `export { Component }`
+// where `Component` is the name of a variable inside
+// the React library.
+
 import QuestionDetails from './QuestionDetails';
 import AnswerDetails from './AnswerDetails';
 import AnswerList from './AnswerList';
 import questionData from '../questionData';
 
 class QuestionShowPage extends Component {
+  constructor (props) {
+    // When class based component is first initialize, the
+    // `props` are passed to the constructor. When inside constructor
+    // and only when inside, you should use `props` without `this.`.
+    super(props);
+    // When overriding the Component's constructor, we must
+    // always use `super(props);` to call the constructor of
+    // the Component class. This configures our component such
+    // as setting the `props` on `this`.
+
+    this.state = {
+      question: questionData
+    };
+  }
+
   render () {
     // To pass props to React elements, set them with
     // "HTML attrbutes" inside JSX. Each attribute will
@@ -18,9 +41,9 @@ class QuestionShowPage extends Component {
           margin: '0 1rem'
         }}
         >
-          <QuestionDetails {...questionData} />
+          <QuestionDetails {...this.state.question} />
           <h3>Answers</h3>
-          <AnswerList answers={questionData.answers} />
+          <AnswerList answers={this.state.question.answers} />
         </main>
       )
   }
