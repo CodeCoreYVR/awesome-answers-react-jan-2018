61370fb046b8e29d18150bfc6870e2f0c0329c48
Steve Godin <steve.god@gmail.com>
2018-03-08 13:13:30 -0800

    Add demo for Question Show to Class

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionDetails from './QuestionDetails';
 import AnswerDetails from './AnswerDetails';
 import AnswerList from './AnswerList';
 import questionData from '../questionData';
 
-function QuestionShowPage () {
-  // To pass props to React elements, set them with
-  // "HTML attrbutes" inside JSX. Each attribute will
-  // act as a property of the component's `props` object.
+class QuestionShowPage extends Component {
+  render () {
+    // To pass props to React elements, set them with
+    // "HTML attrbutes" inside JSX. Each attribute will
+    // act as a property of the component's `props` object.
 
-  // 1rem is == to the font-size of the root tag (<html> ...).
-  return (
-    <main
-      className="QuestionShowPage"
-      style={{
-        margin: '0 1rem'
-      }}
-    >
-      <QuestionDetails {...questionData} />
-      <h3>Answers</h3>
-      <AnswerList answers={questionData.answers} />
-    </main>
-  )
+    // 1rem is == to the font-size of the root tag (<html> ...).
+    return (
+      <main
+        className="QuestionShowPage"
+        style={{
+          margin: '0 1rem'
+        }}
+        >
+          <QuestionDetails {...questionData} />
+          <h3>Answers</h3>
+          <AnswerList answers={questionData.answers} />
+        </main>
+      )
+  }
 }
 
 export default QuestionShowPage;
