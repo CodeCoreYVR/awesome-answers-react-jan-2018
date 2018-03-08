070b429fb7de3e42bf7b77a04d54dcd03524d29a
Steve Godin <steve.god@gmail.com>
2018-03-07 16:01:49 -0800

    Add demo for Index of Questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import QuestionShowPage from './QuestionShowPage';
+import QuestionIndexPage from './QuestionIndexPage';
 
 // When building React applications, we create
 // a root component that is the ancestor to all the
@@ -8,7 +9,10 @@ import QuestionShowPage from './QuestionShowPage';
 // For this application, the `App` serves that role.
 function App () {
   return (
-    <QuestionShowPage />
+    <div className="App">
+      <QuestionIndexPage />
+      <QuestionShowPage />
+    </div>
   )
 }
 
---
+++ src/components/QuestionIndexPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Field from './Field';
+import questionsData from '../questionsData';
+
+function QuestionIndexPage () {
+  return (
+    <main
+      className="QuestionIndexPage"
+      style={{margin: '0 1rem'}}
+    >
+      <h2>Questions</h2>
+      <ul>
+        {
+          questionsData.map(
+            question => (
+              <li>
+                <a href="">{question.title}</a>
+                <Field name="Author" value={question.author.full_name} />
+              </li>
+            )
+          )
+        }
+      </ul>
+    </main>
+  )
+}
+
+export default QuestionIndexPage;
