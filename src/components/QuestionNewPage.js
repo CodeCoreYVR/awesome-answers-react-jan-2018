71d939e90bfed6d05b8ec98b7ddc86cab6b4f4c0
Steve Godin <steve.god@gmail.com>
2018-03-09 16:26:07 -0800

    Add demo for Create Question

--- src/components/QuestionNewPage.js
+++ src/components/QuestionNewPage.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import QuestionForm from './QuestionForm';
+import { Question } from '../lib/requests';
 
 class QuestionNewPage extends Component {
+  constructor (props) {
+    super(props);
+    this.createQuestion = this.createQuestion.bind(this);
+  }
+
+  createQuestion (questionParams) {
+    Question
+      .create(questionParams)
+      .then(data => {
+        // const id = data.id
+        const { id } = data;
+
+        // Components rendered by the <Route /> component
+        // gain access to a .history than can be used to manipulate
+        // history. Using allows to redirect a user to
+        // a different rendering whichever component is there.
+        this.props.history.push(`/questions/${id}`);
+      })
+  }
+
   render () {
     return (
       <main
@@ -9,7 +30,9 @@ class QuestionNewPage extends Component {
         style={{margin: '0 1rem'}}
       >
         <h1>New Question</h1>
-        <QuestionForm />
+        <QuestionForm
+          onSubmit={this.createQuestion}
+        />
       </main>
     );
   }
